514bcd5ca77f0867dccbe52e0d238ac3276c645e
rcrmn <rcrmn16@gmail.com>
2016-05-15 13:27:49 +0200

    Added ObjPtr to the sel namespace

--- include/selene/ObjPtr.h
+++ include/selene/ObjPtr.h
@@ -1,49 +1,52 @@
 #pragma once
 #include <type_traits>
 
-template <typename T>
-class ObjPtr
+namespace sel
 {
-	T* _ptr;
-	bool _managed;
-public:
-	ObjPtr( T* ptr )
-		: _ptr(ptr)
-		, _managed(false)
+	template <typename T>
+	class ObjPtr
 	{
-	}
-
-	ObjPtr( T* ptr, bool managed )
-		: _ptr(ptr)
-		, _managed(managed)
-	{
-	}
-
-	ObjPtr( ObjPtr<T>&& obj )
-		: _ptr(obj._ptr)
-		, _managed(obj._managed)
-	{
-		obj._ptr = nullptr;
-		obj._managed = false;
-	}
-
-	void release()
-	{
-		if ( _managed )
+		T* _ptr;
+		bool _managed;
+	public:
+		ObjPtr( T* ptr )
+			: _ptr( ptr )
+			, _managed( false )
 		{
-			delete _ptr;
-			_managed = false;
 		}
-		_ptr = nullptr;
-	}
 
-	T* operator->() const
-	{
-		return _ptr;
-	}
+		ObjPtr( T* ptr, bool managed )
+			: _ptr( ptr )
+			, _managed( managed )
+		{
+		}
 
-	T* get() const
-	{
-		return _ptr;
-	}
-};
+		ObjPtr( ObjPtr<T>&& obj )
+			: _ptr( obj._ptr )
+			, _managed( obj._managed )
+		{
+			obj._ptr = nullptr;
+			obj._managed = false;
+		}
+
+		void release()
+		{
+			if ( _managed )
+			{
+				delete _ptr;
+				_managed = false;
+			}
+			_ptr = nullptr;
+		}
+
+		T* operator->() const
+		{
+			return _ptr;
+		}
+
+		T* get() const
+		{
+			return _ptr;
+		}
+	};
+}
